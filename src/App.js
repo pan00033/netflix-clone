e115d3d61386544e607f1fc2f78b87219c548b37
ArizonaPeachTea <zuxingp888@gmail.com>
2021-02-26 22:47:06 -0600

    set up Row component css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import Row from "./Components/Row";
+import requests from "./request";
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix front-end clone</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
